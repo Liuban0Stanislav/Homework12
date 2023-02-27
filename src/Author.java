56eb5f217ff37c678e3cc6bf69e60f9a247faff9
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-02-27 23:30:44 +1000

    Домашка готова

---
+++ src/Author.java
@@ -0,0 +1,14 @@
+public class Author {
+    public String author;
+
+    public Author (String author){
+        this.author = author;
+    }
+
+    public String getName(){
+        return author;
+    }
+    public void setName (String author){
+        this.author = author;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Author kirBulichev = new Author("Кир Булычев");
+        Author nikolayNosov = new Author("Николай Носов");
 
+        Book tayna3Planety = new Book("Тайна третьей планеты", kirBulichev, 2013);
+        Book nethnaykaNaLune = new Book("Незнайка на луне", nikolayNosov, 1965);
+
+        System.out.println("Название книги = " + tayna3Planety.getName());
+        System.out.println("Автор = " + tayna3Planety.getAuthor().getName());
+        System.out.println("Год издания = " + tayna3Planety.getPublishYear());
+        System.out.println();
+
+        nethnaykaNaLune.setPublishYear(1996);
+        System.out.println("Год издания измененный = " + nethnaykaNaLune.getPublishYear());
     }
 }
