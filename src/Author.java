b94de22f09879b8d0ecb6a5a0440d9dc21595fcd
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-03-02 19:35:45 +1000

    Домашка 13 готова

--- src/Author.java
+++ src/Author.java
@@ -1,14 +1,45 @@
+import java.util.Objects;
+
 public class Author {
-    public String author;
+    private String firstName;
+    private String lastName;
 
-    public Author (String author){
-        this.author = author;
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public String getName(){
-        return author;
+    public String getFirstName() {
+        return firstName;
     }
-    public void setName (String author){
-        this.author = author;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Автор: " + firstName + " " + lastName;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Author kirBulichev = new Author("Кир Булычев");
-        Author nikolayNosov = new Author("Николай Носов");
+        Author kirBulichev = new Author("Кир", "Булычев");
+        Author nikolayNosov = new Author("Николай", "Носов");
 
         Book tayna3Planety = new Book("Тайна третьей планеты", kirBulichev, 2013);
         Book nethnaykaNaLune = new Book("Незнайка на луне", nikolayNosov, 1965);
 
         System.out.println("Название книги = " + tayna3Planety.getName());
-        System.out.println("Автор = " + tayna3Planety.getAuthor().getName());
+        System.out.println("Автор = " + tayna3Planety.getAuthor().getFirstName() + " " + tayna3Planety.getAuthor().getLastName());
         System.out.println("Год издания = " + tayna3Planety.getPublishYear());
         System.out.println();
 
         nethnaykaNaLune.setPublishYear(1996);
         System.out.println("Год издания измененный = " + nethnaykaNaLune.getPublishYear());
+        System.out.println();
+
+        System.out.println(kirBulichev.toString());
+        System.out.println(kirBulichev.equals(nikolayNosov));
+        System.out.println(kirBulichev.hashCode());
+        System.out.println();
+
+        System.out.println(tayna3Planety.toString());
+        Book newBook = nethnaykaNaLune;
+        System.out.println(nethnaykaNaLune.equals(newBook));
+        System.out.println(nethnaykaNaLune.hashCode());
     }
 }
